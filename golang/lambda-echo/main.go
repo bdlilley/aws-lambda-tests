d07ffb046795d8d8909e24eec0ff93cc0dda26bb
bdlilley <benji.lilley@solo.io>
2022-10-22 12:28:59 -0400

    wip

--- golang/lambda-echo/main.go
+++ golang/lambda-echo/main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+type DiagnosticResponse struct {
+	Message string `json:"message"`
+}
+
+func HandleLambdaEvent(event interface{}) (DiagnosticResponse, error) {
+	return DiagnosticResponse{Message: "todo"}, nil
+}
+
 func main() {
-	
+	lambda.Start(HandleLambdaEvent)
 }
